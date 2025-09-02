06a7fdf8e7cc387dba3df727337252a95b05efdb
atishblote <83937743+atishblote@users.noreply.github.com>
2025-09-02 19:20:43 +0530

    Update frontendRoutes.js
    limit routes

--- api/routes/frontendRoutes.js
+++ api/routes/frontendRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const frontend  = require('../controllers/frontendController')
 
 router.get('/skills', frontend.getSkills)
-router.get('/projects', frontend.getProjects)
+router.get('/projects?limit=4', frontend.getProjects)
 router.get('/projects/filter/:id', frontend.getFilterProjects)
 router.get('/projects/filter', frontend.getQueryProjects)
 
